f68defe9e34455ae83a04dfccb5d1cd1d394d8d4
Manuel Montenegro <manuel@nucypher.com>
2023-06-08 11:52:14 +0200

    Add tests for RPC calls

--- tests/test_call.py
+++ tests/test_call.py
@@ -1,6 +1,8 @@
 from multicall import Call
+from multicall.constants import Network, MULTICALL3_ADDRESSES
 
 CHAI = "0x06AF07097C9Eeb7fD685c692751D5C66dB49c215"
+MULTICALL3 = MULTICALL3_ADDRESSES[Network.Mainnet]
 
 
 def from_wei(value):
@@ -20,5 +22,22 @@ def test_call_with_args(w3):
 def test_call_with_predefined_args(w3):
     call = Call(
         CHAI, ["balanceOf(address)(uint256)", CHAI], [["balance", from_wei]], w3=w3
+    )
+    assert isinstance(call()["balance"], float)
+
+
+def test_call_get_block_number(w3):
+    call = Call(
+        MULTICALL3, "getBlockNumber()(uint256)", [["block_number", None]], w3=w3
+    )
+    assert isinstance(call()["block_number"], int)
+
+
+def test_call_get_eth_balance(w3):
+    call = Call(
+        MULTICALL3,
+        ["getEthBalance(address)(uint256)", CHAI],
+        [["balance", from_wei]],
+        w3=w3,
     )
     assert isinstance(call()["balance"], float)
--- tests/test_multicall.py
+++ tests/test_multicall.py
@@ -1,8 +1,11 @@
 from typing import Any, Tuple
 
 from multicall import Call, Multicall
+from multicall.constants import Network, MULTICALL3_ADDRESSES
+
 
 CHAI = "0x06AF07097C9Eeb7fD685c692751D5C66dB49c215"
+MULTICALL3 = MULTICALL3_ADDRESSES[Network.Mainnet]
 DUMMY_CALL = Call(CHAI, "totalSupply()(uint)", [["totalSupply", None]])
 
 
@@ -27,7 +30,6 @@ def test_multicall(w3):
         ],
     )
     result = multi()
-    print(result)
     assert isinstance(result["supply"], float)
     assert isinstance(result["balance"], float)
 
@@ -50,6 +52,18 @@ def test_multicall_no_success(w3):
         require_success=False,
     )
     result = multi()
-    print(result)
     assert isinstance(result["success"], tuple)
     assert isinstance(result["balance"], tuple)
+
+
+def test_multicall_token_balance_and_block_number(w3):
+    multi = Multicall(
+        w3,
+        [
+            Call(CHAI, ["balanceOf(address)(uint256)", CHAI], [["balance", from_ray]]),
+            Call(MULTICALL3, "getBlockNumber()(uint256)", [["block_number", None]]),
+        ],
+    )
+    result = multi()
+    assert isinstance(result["balance"], float)
+    assert isinstance(result["block_number"], int)
